d2cd46e123c005dca655c376f7461154b9c54633
Sam Montoya <smon7958@740s-pl-118.local>
2016-02-24 12:11:01 -0700

    Added to Repo

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,19 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: smon7958
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout <<"This is C? Gross." << endl;
+	//This is like System.out.println in Java.
+
+	return 0;
+}
+
+
